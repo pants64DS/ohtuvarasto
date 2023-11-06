e1fcedb8d89b955e604fef9b30c7f3079ea6e5ce
pants64DS <pants64DS@users.noreply.github.com>
2023-11-06 22:49:09 +0200

    Korjaa koodi

--- src/varasto.py
+++ src/varasto.py
@@ -16,8 +16,6 @@ class Varasto:
             # täyteen ja ylimäärä hukkaan!
             self.saldo = tilavuus
 
-        self.tilavuus = ""
-
     # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms.
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
